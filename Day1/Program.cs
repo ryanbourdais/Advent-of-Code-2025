2c80a8b900ac6ecc3a02979476d16fd85fda3a40
Ryan Bourdais <ryan.bourdais@birdon.us>
2025-12-02 12:24:26 -0600

    first commit

---
+++ Day1/Program.cs
@@ -0,0 +1,80 @@
+﻿// List<int> numbers = new List<int>();
+// for(int i = 0; i <= 99; i++)
+// {
+//     numbers.Add(i);
+// }
+
+// int ActiveNumber = numbers[50];
+
+string filePath = "input.txt";
+
+List<Combination> combinations = new List<Combination>();
+
+try
+{
+    Console.WriteLine($"Reading file: {filePath}");
+    int lineCount = 0;
+    foreach (string line in File.ReadLines(filePath))
+    {
+        char direction = line[0];
+        int count = int.Parse(line[1..]);
+        combinations.Add(new Combination{direction = direction, count = count});
+        lineCount++;
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+}
+
+int Position = 50;
+int Zeroes = 0;
+foreach(Combination rotation in combinations)
+{
+    int localRotation = rotation.count;
+    int direction = 1;
+    if(rotation.direction == 'L')
+    {
+        localRotation *= -1;
+        direction = -1;
+    }
+    int start = Position;
+    Position += localRotation;
+    Position %= 100;
+    if (Position < 0 ) Position += 100;
+    int steps = Math.Abs(localRotation % 100);
+    Zeroes += (int)(MathF.Abs(localRotation) / 100);
+    if(MathF.Abs(localRotation) % 100 != 0)
+    if(start == 0 && localRotation <= 99) continue;
+    {
+        if(direction == -1 && Position != 0)
+        {
+            int distToZeroLeft = start;
+            if(steps > distToZeroLeft) Zeroes++;
+        }
+        else if(Position != 0 && direction == 1)
+        {
+            int distToZeroRight = (100 - start);
+            if (steps > distToZeroRight) Zeroes++;
+        }
+    }
+    if(Position == 0)
+    {
+        Zeroes++;
+    }
+
+
+    // Step 1
+    // if(Position % 100 == 0)
+    // {
+    //     Zeroes++;
+    // }
+
+}
+
+Console.WriteLine(Zeroes);
+class Combination
+{
+    public char direction { get; set; }
+    public int count { get; set;}
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+string filePath = "input.txt";
+
+List<ulong> ids = new List<ulong>();
+try
+{
+    using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                // Read the file line by line until the end
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Split the line by the comma delimiter
+                    string[] fields = line.Split(',');
+
+                    // Process each field (e.g., print to console)
+                    foreach (string field in fields)
+                    {
+                        ulong start = ulong.Parse(field.Split('-')[0]);
+                        ulong end = ulong.Parse(field.Split('-')[1]);
+                        ulong current = start;
+                        while (current != end + 1)
+                        {
+                            ids.Add(current);
+                            current++;
+                        }
+                    }
+                }
+            }
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+}
+
+ulong total = 0;
+foreach( ulong id in ids)
+{
+    string idString = id.ToString();
+    int len = idString.Length;
+    int midIndex = Math.Abs(len / 2);
+    for(int i = 1; i <= midIndex; i++)
+    {
+        if (len % i != 0) continue;
+        List<string> splitParts = new List<string>();
+        int partLength = i;
+        for (int j = 0; j < idString.Length; j += partLength)
+        {
+            int length = Math.Min(partLength, idString.Length - j);
+            splitParts.Add(idString.Substring(j, length));
+        }
+        if(splitParts.Distinct().Count() == 1)
+        {
+            Console.WriteLine(id);
+            total += (ulong)id;
+            break;
+        }
+    }
+    //Part 1
+    // string subString1 = idString.Substring(0, midIndex);
+    // string subString2 = idString.Substring(midIndex);
+    // if(subString1 == subString2)
+    // {
+    //     Console.WriteLine(subString1 + " " + subString2);
+    //     total += (ulong)id;
+    // }
+}
+
+Console.WriteLine(total);
